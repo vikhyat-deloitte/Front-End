e6086d94f0d84a84d96aaf249a774f00c4698e4d
vikhyat-deloitte <128490528+vikhyat-deloitte@users.noreply.github.com>
2023-04-20 03:33:10 +0530

    Add files via upload
    Created dynamic fetch for status,types and priorities

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import BaseLayout from "@/components/layout";
+import IssueForm from "@/components/IssueForm";
+function ShowIssueCreate() {
+    const passADiv = <div>Pass any div here, you must pass it!</div>;
+    return (
+        <IssueForm/>
+    );
+}
+
+export default ShowIssueCreate;
